e0965984c0a5ef0991b35cee52c2e6a9325c4dae
JUSTO <JUSTO@host.docker.internal>
2025-06-04 15:57:25 +0800

    Create Service Registry. Closes #2

---
+++ services/ecomm-service-registry/src/test/java/com/apj/ecomm/registry/EcommServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apj.ecomm.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
